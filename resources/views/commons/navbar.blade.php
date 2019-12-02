539325734ea64d97317c1be5e7e9d2b3a59c33a5
ryoko.kosugi <55273342+ryoko-kosugi@users.noreply.github.com>
2019-12-02 11:03:14 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,9 +8,9 @@
         
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav">
-                <li class="nav-item"><a href="#" class="nav-link">Sign up</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">Log in</a></li>
+            <ul class="nav navbar-nav navbar-right">
+                <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                <li><a href="#">Login</a></li>
             </ul>
         </div>
     </nav>
